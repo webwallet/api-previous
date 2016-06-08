b895c51b744ed0af01aabeb1e9a7fc620305bd63
Jorge Zaccaro <jorgezaccaro@gmail.com>
2016-06-08 16:46:55 -0500

    Abstracted controller module loading

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,15 +1,37 @@
 'use strict';
 
 const handleHttp = require('./handleHttp');
-
-/* Handlers */
 const transaction = require('./transaction');
 
 /* Controllers */
-const controllers = {
-  http: {
-    'transaction.post': handleHttp(transaction.post)
-  }
+const controllers = [
+  'transaction.post'
+];
+
+const rootEnpoints = {
+  transaction
 };
 
-module.exports = controllers;
+/**
+ *
+ */
+function setupControllers(wrapper, paths) {
+  return paths.reduce((reduced, key) => {
+    return (reduced[key] = wrapper(findController(key))) && reduced;
+  }, {});
+}
+
+/**
+ *
+ */
+function findController(path) {
+  let [root, ...segments] = path.split('.');
+  let controller = segments
+    .reduce((reduced, segment) => reduced[segment], rootEnpoints[root]);
+
+  return controller;
+}
+
+module.exports = {
+  http: setupControllers(handleHttp, controllers)
+};
