465465a96c6b114eac0ac65f6081f5e8977f2379
Jorge Zaccaro <jorgezaccaro@gmail.com>
2016-06-20 18:05:04 -0500

    Added module for building subdocument paths

---
+++ common/database/paths.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+    address: {
+      transaction: {
+        pointer: ({ index = 0 }) => {
+          return `pointers[${index}]`;
+        }
+      }
+    },
+    transaction: {
+      output: ({ index = 0 }) => {
+        return `payload.outputs[${index}]`;
+      }
+    }
+};
