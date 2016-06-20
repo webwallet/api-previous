ee745f18bab161f3463e4dc9d3724d600e2ee4c3
Jorge Zaccaro <jorgezaccaro@gmail.com>
2016-06-20 18:04:43 -0500

    Added module for building database keys

---
+++ common/database/keys.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+    address: {
+      transactions: {
+        index: ({ address }) => {
+          return `adr::${address}::txs`;
+        }
+      }
+    },
+    transaction: {
+      record: ({ hash }) => {
+        return `tra::${hash}`;
+      }
+    }
+};
