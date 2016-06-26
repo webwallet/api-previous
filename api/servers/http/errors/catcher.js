a89765ea3e9254e690f77af6fb4a3e3e1777ac1b
Jorge Zaccaro <jorgezaccaro@gmail.com>
2016-06-25 19:09:53 -0500

    refactor(http/errors): catch json.parse

--- api/servers/http/errors/catcher.js
+++ api/servers/http/errors/catcher.js
@@ -6,11 +6,9 @@ const status = require('*common/http/status-codes.json');
  *
  */
 function handleErrors(error, request, response, next) {
-  return response.status(status.badRequest).json({
-    errors: [
-      {status: status.badRequest, name: 'bad-request', details: error}
-    ]
-  }).end();
+  let err = {status: status.badRequest, name: 'bad-request', details: {}};
+  if (error.body) err.details.body = 'invalid-json';
+  return response.status(status.badRequest).json({errors: [err]}).end();
 }
 
 module.exports = handleErrors;
