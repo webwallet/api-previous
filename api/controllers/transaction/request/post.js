0f4121799c0b458d045e71a16cf355577355fda7
Jorge Zaccaro <jorgezaccaro@gmail.com>
2016-07-11 15:47:42 -0500

    feat(transaction/post): insert transaction record - buildTransactionRecord. - insertTransactionPointers. - insertTransactionRecord.

--- api/controllers/transaction/request/post.js
+++ api/controllers/transaction/request/post.js
@@ -12,7 +12,10 @@ const {
   getTransactionCounters,
   getTransactionPointers,
   getTransactionOutputs,
-  computeTransactionOutputs
+  computeTransactionOutputs,
+  buildTransactionRecord,
+  insertTransactionPointers,
+  insertTransactionRecord
 } = require('./lib');
 
 /**
@@ -27,15 +30,22 @@ function * postTransactionRequest(request) {
   let inputs = transaction.inputs = body.data.inputs;
   // validate that transaction inputs have not been cleared before
 
+  /* Parse involved addresses and currencies, then retrieve counters, pointers and outputs  */
   let addresses = parseTransactionAddresses({db, transaction: body.data});
   let currencies = parseTransactionCurrencies({db, transaction: body.data});
   previous.counters = yield getTransactionCounters({db, currencies});
   previous.pointers = yield getTransactionPointers({db, addresses});
   previous.outputs = yield getTransactionOutputs({db, addresses, pointers: previous.pointers});
 
+  /* Compute transaction outputs and build transaction record */
   let computeOutputsParams = {db, addresses, currencies, inputs, previous};
-  transaction.outputs = computeTransactionOutputs(computeOutputsParams);
-  // create transaction record
+  let outputs = transaction.outputs = computeTransactionOutputs(computeOutputsParams);
+  let record = buildTransactionRecord({transaction});
+
+  /* Insert transaction pointers in all addresses' indexes and insert transaction record */
+  let pointersParams = {db, addresses, outputs, hash: record.hash.val};
+  let pointersResult = yield insertTransactionPointers(pointersParams);
+  let recordResult = yield insertTransactionRecord({db, currencies, previous, record});
 
   return response.post(body);
 }
